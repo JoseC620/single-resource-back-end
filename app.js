402ec28ad91464ec3222dc7acf0a490a8a287428
JoseC620 <Jcepeda6201@gmail.com>
2023-05-08 19:17:52 -0400

    server up and running

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan")
+
+app.use(express.json())
+app.use(cors());
+
+app.use(logger('dev'))
+
+app.get(("/"), ( req, res ) => {
+    res.send("welcome to the snacks")
+});
+
+app.get("*", ( req, res ) => {
+    res.status(404).json( { error: "Page not found" } )
+});
+
+module.exports = app;
