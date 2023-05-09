ecbec29225f53d62eb857bc0c93e5d00c29b69eb
shareekaepps <shareekaepps@icloud.com>
2023-05-09 19:39:42 -0400

    snacks queries, snacksController updated and functional

--- controllers/snacksController.js
+++ controllers/snacksController.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const snacks = express.Router();
-
-const { getAllSnacks } = require("../queries/snacks");
+const { getAllSnacks, getSnack, createSnack, deleteSnack, updateSnack } = require('../queries/snacks');
 
 snacks.get("/", async ( req, res ) => {
     const allSnacks = await getAllSnacks();
@@ -11,6 +10,52 @@ snacks.get("/", async ( req, res ) => {
         res.status(500).json({ error: "server error" });
     }
 });
+
+snacks.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const snacks = await getSnack(id);
+
+    if(snacks) {
+        res.json(snacks);
+    } else {
+        res.status(404).json({ error: "Sorry! Snack was not found" })
+    }
+});
+
+snacks.post('/', async (req, res) => {
+    const newSnack = req.body;
+
+    try {
+        const addedSnack = await createSnack(newSnack);
+        res.status(200).json(addedSnack)
+    } catch (error) {
+        res.status(400).json({ error: error })
+    }
+});
+   
+snacks.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const removed = await deleteSnack(id);
+    if(deleteSnack.id) {
+        res.status(200).json(removed);
+    } else {
+        res.status(404).json({ error: "Sorry! Snack was not found" });
+    }
+});
+
+snacks.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+
+    try {
+        const updated = await updateSnack(id, body);
+        res.status(200).json(updated);
+    } catch (error){
+        res.status(400).json({ error: "Sorry! Snack could not be updated" });
+    }
+});
+
+
 
 
 module.exports = snacks; 
